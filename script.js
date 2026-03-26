137c5afab83efa7603f665368a1105e1121b4b3a
Edin-Dev <nurhaedin18@gmail.com>
2026-03-26 14:29:55 +0700

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const dataBarang = [
         { 
             nama: "majmu'atudawah", 
             harga: "Rp 30.000 " + e,
-            link: "https://vt.tokopedia.com/t/ZS9R3Es4uf6ys-jtkd8/", 
+            link: "https://vt.tokopedia.com/t/ZS9R3oWWhBCYq-Atvb1/", 
             foto: "img/majmu.png" 
         },
        
